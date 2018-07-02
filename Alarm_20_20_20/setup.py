bb60803cee3bcef33d430c7ccefd2cd27d49dd22
Shantanu Saini <shantanu1395@gmail.com>
2018-07-03 01:01:18 +0530

    deleted alarm and  other scripts

--- Alarm_20_20_20/setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup,Executable
-setup(
-    name = "alarm_20_20_20",
-    version = "0.1",
-    description = "Utility",
-    executables = [Executable("myalarm_20_20_20.py")])
